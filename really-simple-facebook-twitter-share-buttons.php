5e7d0fc91c8639b88054473cb4ad69c0f2876b91
tanaylakhani <tanaylakhani@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-02-25 18:10:13 +0000

    add author name
    git-svn-id: https://plugins.svn.wordpress.org/really-simple-facebook-twitter-share-buttons/tags/4.0@1099446 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- really-simple-facebook-twitter-share-buttons.php
+++ really-simple-facebook-twitter-share-buttons.php
@@ -3,7 +3,7 @@
 Plugin Name: Really Simple Share
 Plugin URI: http://www.whiletrue.it/really-simple-facebook-twitter-share-buttons-for-wordpress/
 Description: Puts Facebook, Twitter, LinkedIn, Google "+1", Pinterest and other share buttons of your choice above or below your posts.
-Author: Dabelon
+Author: Dabelon, tanaylakhani
 Version: 4.0
 Author URI: http://www.whiletrue.it
 */
