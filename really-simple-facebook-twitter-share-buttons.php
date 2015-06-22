15794d86352676d92d3166da538a64b485c72275
tanaylakhani <tanaylakhani@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-06-22 15:56:06 +0000

    * Bug Fix for readygraph
    git-svn-id: https://plugins.svn.wordpress.org/really-simple-facebook-twitter-share-buttons/trunk@1185634 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- really-simple-facebook-twitter-share-buttons.php
+++ really-simple-facebook-twitter-share-buttons.php
@@ -4,7 +4,7 @@ Plugin Name: Really Simple Share
 Plugin URI: http://www.whiletrue.it/really-simple-facebook-twitter-share-buttons-for-wordpress/
 Description: Puts Facebook, Twitter, LinkedIn, Google "+1", Pinterest and other share buttons of your choice above or below your posts.
 Author: Dabelon, tanaylakhani
-Version: 4.3.4
+Version: 4.3.5
 Author URI: http://www.readygraph.com
 */
 
